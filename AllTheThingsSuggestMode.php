039160a631bc39d2950c867076df1e0bdb146146
jsblair9 <james@lionsmouth.digital>
2019-03-07 11:54:36 -0600

    :hatching_chick:

---
+++ AllTheThingsSuggestMode.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Statamic\Addons\HoneyBooBoo;
+
+use Statamic\Addons\Suggest\Modes\AbstractMode;
+use Statamic\API\Entry;
+use Statamic\API\Page;
+
+/**
+ * A combination of pages and collection suggest modes.
+ * Code mostly stolen from core.
+ */
+class AllTheThingsMode extends AbstractMode
+{
+    public function suggestions()
+    {
+        $suggestions = [];
+
+        // Add pages to suggestions.
+        // ---------------------------------------------------------------------
+        // If a parent has been specified, get it's child pages at
+        // the specified depth. Otherwise, just get all pages.
+        if ($parent = $this->request->input('parent')) {
+            $parent = (Str::startsWith($parent, '/')) ? Page::whereUri($parent) : Page::find($parent);
+            $pages = $parent->children($this->request->input('depth'));
+        } else {
+            $pages = Page::all();
+        }
+
+        $pages = $pages->multisort($this->request->input('sort', 'title:asc'));
+
+        foreach ($pages as $page) {
+            $suggestions[] = [
+                'value' => $page->id(),
+                'text'  => $this->label($page, 'title')
+            ];
+        }
+
+
+
+
+        // Add entries to suggestions.
+        // ---------------------------------------------------------------------
+        $collection = $this->request->input('collection');
+        // If no specified collection, just get all entries.
+        $entries    = $collection ? Entry::whereCollection($collection) : Entry::all();
+        $entries    = $entries->multisort($this->request->input('sort', 'title:asc'));
+
+        foreach ($entries as $entry) {
+            $suggestions[] = [
+                'value' => $entry->id(),
+                'text'  => $this->label($entry, 'title')
+            ];
+        }
+
+        return $suggestions;
+    }
+}
